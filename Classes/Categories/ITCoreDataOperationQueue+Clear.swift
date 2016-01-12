86b6a701376ac965e1c85ca66b12794c3ce4db74
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2016-01-12 12:36:38 +0300

    ported clear category

---
+++ Classes/Categories/ITCoreDataOperationQueue+Clear.swift
@@ -0,0 +1,44 @@
+//
+//  ITCoreDataOperationQueue+Clear.swift
+//  CoreDataStack-Swift
+//
+//  Created by Aliaksandr Skulin on 1/12/16.
+//  Copyright © 2016 Aliaksandr Skulin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+extension ITCoreDataOperationQueue {
+    
+    func clearAllEntities(completion:(() -> Void)?) {
+        self.changesContext.performBlock { () -> Void in
+            let allEntities: NSArray = self.model.entities
+            allEntities.enumerateObjectsUsingBlock({ (entityDescription, idx, stop) -> Void in
+                let request: NSFetchRequest = NSFetchRequest(entityName: entityDescription.name)
+                let results: NSArray
+                do {
+                    results = try self.changesContext.executeFetchRequest(request)
+                } catch {
+                    //TODO: add logging
+                    if (completion != nil) {
+                        completion!()
+                    }
+                    return
+                }
+                for (object) in results {
+                    self.changesContext.delete(object)
+                }
+            })
+            do {
+                try self.changesContext.save()
+            } catch {
+                //TODO: add logging
+            }
+            if (completion != nil) {
+                completion!()
+            }
+        }
+    }
+
+}
