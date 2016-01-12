1ce16ffd056dec958643734b5a4e978bf5278f53
Aliaksandr Skulin <aliaksandr.skulin@instinctools.ru>
2016-01-12 10:58:10 +0300

    Added project

---
+++ Classes/CoreDataStack-Swift.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStack-Swift.h
+//  CoreDataStack-Swift
+//
+//  Created by Aliaksandr Skulin on 1/12/16.
+//  Copyright © 2016 Aliaksandr Skulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataStack-Swift.
+FOUNDATION_EXPORT double CoreDataStack_SwiftVersionNumber;
+
+//! Project version string for CoreDataStack-Swift.
+FOUNDATION_EXPORT const unsigned char CoreDataStack_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStack_Swift/PublicHeader.h>
+
+
